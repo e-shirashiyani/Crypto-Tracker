92c071bb980826e611fe287b219fb2d815d3ad2d
e.shirashiyani <e.shirashiyani>
2024-01-29 10:09:22 +0330

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by e.shirashiyani on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
